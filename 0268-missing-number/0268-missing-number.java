341a6d19de2698fc7c77944cf24a0485d07d03b7
DUDI DINESH <208412574+Dinesh2006-Dudi@users.noreply.github.com>
2026-09-07 18:30:22 +0530

    Time: 0 ms (100%), Space: 47.1 MB (91.47%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int expsum=0;
+        int actsum=0;
+
+        for(int i=0;i<n;i++)
+        {
+            actsum+=nums[i];
+        }
+        expsum=(n*(n+1))/2;
+        return expsum-actsum;
+
+        
+    }
+}
